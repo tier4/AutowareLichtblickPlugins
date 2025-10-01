d44c04a8893f47b3699c09714069c8b5f2799c3e
Jacob Lambert <jacob.lambert.2@tier4.jp>
2025-10-01 16:24:25 +0900

    split DetectedObjects and DetectedObject types for reusability

--- src/msgs/perception/DetectedObjects.ts
+++ src/msgs/perception/DetectedObjects.ts
@@ -8,29 +8,31 @@ import { Twist } from "../base/Twist";
 
 export type DetectedObjects = {
   header: Header;
-  objects: {
-    existence_probability: number;
-    classification: ClassificationLabel[];
-    kinematics: {
-      pose_with_covariance: {
-        pose: Pose;
-        covariance: Float64Array;
-      };
-      has_position_covariance: boolean;
-      orientation_probability: number;
-      twist_with_covariance: {
-        twist: Twist;
-        covariance: Float64Array;
-      };
-      has_twist: boolean;
-      has_twist_covariance: boolean;
+  objects: DetectedObject[];
+};
+
+export type DetectedObject = {
+  existence_probability: number;
+  classification: ClassificationLabel[];
+  kinematics: {
+    pose_with_covariance: {
+      pose: Pose;
+      covariance: Float64Array;
     };
-    shape: {
-      type: number;
-      footprint: {
-        points: Point2D[];
-      };
-      dimensions: Dimensions;
+    has_position_covariance: boolean;
+    orientation_probability: number;
+    twist_with_covariance: {
+      twist: Twist;
+      covariance: Float64Array;
     };
-  }[];
+    has_twist: boolean;
+    has_twist_covariance: boolean;
+  };
+  shape: {
+    type: number;
+    footprint: {
+      points: Point2D[];
+    };
+    dimensions: Dimensions;
+  };
 };
